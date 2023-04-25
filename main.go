00a5f7c48cd9b85067a1f00736c4d814fbb13070
Terry Miller <temiller@linkedin.com>
2023-04-25 15:20:02 -0700

    FizzBuzz Update #1

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 100; i++ {
+
+		if i%3 == 0 {
+			// Multiple of 3
+			fmt.Printf("fizz")
+		}
+		if i%5 == 0 {
+			fmt.Printf("buzz")
+		}
+		if i%3 != 0 && i%5 != 0 {
+			// Neither, so print the number itself
+			fmt.Printf("%d", i)
+		}
+		fmt.Printf("\n")
+	}
+}
